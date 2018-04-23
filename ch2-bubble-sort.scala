6304eb1038be68079960667995e65c7db396dd32
Boris Korogvich <b.korogvich@gmail.com>
2018-04-23 23:39:19 +0300

    add compare func to bubble sort

--- ch2-bubble-sort.scala
+++ ch2-bubble-sort.scala
@@ -1,19 +1,19 @@
 object Main {
   def main(args: Array[String]) = {
     println(bubbleSort(Array(5, 2, 3, 6, 7, 10)).mkString(" "))
-    println(bubbleSort(Array(10, 1)).mkString(" "))
+    println(bubbleSort(Array(1, 10), _ < _).mkString(" "))
 
     println(bubbleSort(Array()).mkString(" "))
   }
 
-  def bubbleSort(array: Array[Int]) = {
+  def bubbleSort(array: Array[Int], comp: (Int, Int) => Boolean = _ > _) = {
     var i = 0
 
     while (i < array.length) {
       var j = 1
 
       while (j < array.length) {
-        if (array(j - 1) > array(j)) {
+        if (comp(array(j - 1), array(j))) {
           val tmp = array(j - 1)
           array(j - 1) = array(j)
           array(j) = tmp
